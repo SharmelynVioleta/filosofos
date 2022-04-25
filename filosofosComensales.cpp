4829b47c5b018e8ffbf0b5bd52c71bc57e3d3824
Sharmelyn Violeta Bautista Luque <sbautistal@ulasalle.edu.pe>
2022-04-25 16:30:49 +0000

    Evidenciando deadlock

--- filosofosComensales.cpp
+++ filosofosComensales.cpp
@@ -2,25 +2,27 @@
 #include<pthread.h>
 #include<stdlib.h>
 #include<unistd.h>
+//#include<iostream>
 
-using namespace std;
+//using namespace std;
 
 #define numFilosofos 5
 
 void *filosofo (void *arg);
+int var = 0;
 
 pthread_mutex_t tenedor[5];
-int var=0;
+
 int main (void){
 	printf("Iniciando programa de filosofos COmensales\n");
-	printf("FILÓSOFOS:\n");
+	printf("\tFILÓSOFOS:\n");
 	pthread_t filosofos[numFilosofos];
 	int id[numFilosofos];
 	int i;
 	
 	
-	printf("variable = %d\n", var);
-	for (int i = 0; i< numFilosofos; i++){
+	//printf("variable = %d\n", var);
+	for (int i = 0; i < numFilosofos; i++){
 		pthread_mutex_init(&tenedor[i], NULL);
 	}
 	for (int i = 0; i < numFilosofos; i++){
@@ -30,7 +32,7 @@ int main (void){
 	for (int i = 0; i < numFilosofos; i++){
 		pthread_join(filosofos[i], NULL);
 	}
-	printf("variable = %d\n", var);
+	//printf("variable = %d\n", var);
 
 	return 0;
 
@@ -38,7 +40,7 @@ int main (void){
 }
 void cogiendoTenedor (int a, int b){
 	pthread_mutex_lock (&tenedor[b]);
-	printf("Filosofo %d cogio tenedor %d \n", a,b);
+	printf("El filósofo número %d está usando el tenedor %d \n", a,b);
 }
 
 
@@ -48,8 +50,8 @@ void soltarTenedor (int s1, int s2){
 }
 
 void comiendo (int arg){
-	int t1 = arg - 1;
-	int t2 = arg - 2;
+	int t1 = arg-1;
+	int t2 = arg-2;
 	if (t2 == -1){
 		t2 = 4;
 	}
@@ -57,13 +59,14 @@ void comiendo (int arg){
 	cogiendoTenedor(arg, t2);
 	printf ("FILÓSOFO %d ESTA COMIENDO \n",arg);
 	int alt = rand() % 10;
-	sleep (alt);
-	soltarTenedor (t1, t2);
+	sleep(alt);
+	soltarTenedor(t1, t2);
 }
 void *filosofo (void *arg){
-	char *arg2 =(char*)arg;
-	for (int i = 0;true; i++){
-		printf("%s Estoy comiendo: %d \n", arg2, i);
+	int arg2 = *((int *)arg);
+	for (int i=1; i <=  5; i++){
+		var = var + 1;
+		comiendo (arg2);
 	}
 	return NULL;	
 }
