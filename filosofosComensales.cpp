7143cd7a3c093bcee7bf20e31cca7aef46ca1d6e
Sharmelyn Violeta Bautista Luque <sbautistal@ulasalle.edu.pe>
2022-04-25 13:51:43 +0000

    Entramos a la seccion crítica

--- filosofosComensales.cpp
+++ filosofosComensales.cpp
@@ -17,20 +17,28 @@ int main (void){
 	int i;
 	
 	
-	printf("variable=%d\n", var);
-	for (int i=0;< numFilosofos; i++){
+	printf("variable = %d\n", var);
+	for (int i = 0; i< numFilosofos; i++){
 		pthread_mutex_init(&tenedor[i], NULL);
 	}
-	for (int i=0; i < numFilosofos; i++){
-		id[i]=i+1;
-		pthread_create(&filosofos[i], NULL, &filosofo, &id[i];
+	for (int i = 0; i < numFilosofos; i++){
+		id[i] = i+1;
+		pthread_create(&filosofos[i], NULL, &filosofo, &id[i]);
 	}
-	for (int i=0; i < numFilosofos; i++){
-		pthread_join(filosofo[i], NULL);
+	for (int i = 0; i < numFilosofos; i++){
+		pthread_join(filosofos[i], NULL);
 	}
+	printf("variable = %d\n", var);
 
 	return 0;
+
+
 }
+void cogiendoTenedor (int fil, int think){
+	pthread_mutex_lock (&tenedor[think]);
+	printf("Filosofo %d cogio tenedor %d \n", fil,think);
+}
+
 void *filosofo (void *arg){
 	char *arg2 =(char*)arg;
 	for (int i = 0;true; i++){
@@ -38,3 +46,4 @@ void *filosofo (void *arg){
 	}
 	return NULL;	
 }
+
